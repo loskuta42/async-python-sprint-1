96167c7ab46abd25359f09742199a06fd1609522
Alexey Lobarev <loskuta42@yandex.ru>
2022-09-29 22:44:22 +0300

    UPD/data calculation class

--- tests.py
+++ tests.py
@@ -1,15 +1,22 @@
 import unittest
 
-from tasks import DataFetchingTask
+from tasks import DataFetchingTask, DataCalculationTask
 from utils import CITIES
 
 
 class MyTest(unittest.TestCase):
+
     def test_data_fetch(self):
         data = DataFetchingTask(CITIES).get_data()
-        print(data)
         info_tzinfo_abbr = data['MOSCOW']['info']['tzinfo']['abbr']
         self.assertEqual(info_tzinfo_abbr, 'MSK')
+
+    def test_data_calculations(self):
+        data = DataFetchingTask(CITIES).get_data()
+        obj = DataCalculationTask(data, '2022-05-26', '2022-05-29')
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os
+
 CITIES = {
     "MOSCOW": "https://code.s3.yandex.net/async-module/moscow-response.json",
     "PARIS": "https://code.s3.yandex.net/async-module/paris-response.json",
@@ -15,6 +17,55 @@ CITIES = {
     "ROMA": "https://code.s3.yandex.net/async-module/roma-response.json",
     "CAIRO": "https://code.s3.yandex.net/async-module/cairo-response.json",
 }
+
+CITIES_RUS = {
+    'ABUDHABI': 'Абу Даби',
+    'BEIJING': 'Пекин',
+    'BERLIN': 'Берлин',
+    'BUCHAREST': 'Бухарест',
+    'CAIRO': 'Каир',
+    'KALININGRAD': 'Калининград',
+    'KAZAN': 'Казань',
+    'LONDON': 'Лондон',
+    'MOSCOW': 'Москва',
+    'NOVOSIBIRSK': 'Новосибирск',
+    'PARIS': 'Париж',
+    'ROMA': 'Рим',
+    'SPETERSBURG': 'Санкт-Петербург',
+    'VOLGOGRAD': 'Волгоград',
+    'WARSZAWA': 'Варшава'
+}
+
+def find_file(name):
+    path = os.getcwd()
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+
+path_to_file = find_file('conditions.txt')
+
+
+def get_bad_conditions_from_file(path_to_file):
+    bad_conditions_words = [
+        'дождь',
+        'град',
+        'снег',
+        'морось',
+        'ливень',
+        'гроза'
+    ]
+    bad_conditions_res = []
+    with open(path_to_file, encoding='utf-8') as file:
+        for line in file:
+            for word in bad_conditions_words:
+                if line.find(word) != -1 and (condition := line.split()[0]) not in bad_conditions_res:
+                    bad_conditions_res.append(condition)
+    return tuple(bad_conditions_res)
+
+
+BAD_CONDITIONS = get_bad_conditions_from_file(path_to_file)
+
 ERR_MESSAGE_TEMPLATE = "Something wrong. Please contact with mentor."
 
 MIN_MAJOR_PYTHON_VER = 3
